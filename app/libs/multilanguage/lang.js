3b97fa198e2434e517c3c54bb65a2f28c6f972b2
Javier <rad222@users.noreply.github.com>
2017-01-16 23:15:41 +0100

    Add multilanguage

---
+++ app/libs/multilanguage/lang.js
@@ -0,0 +1,26 @@
+var langs = ['es', 'en'];
+var langCode = '';
+var langJS = null;
+
+
+var translate = function (jsdata)
+{	
+	$("[tkey]").each (function (index)
+	{
+		var strTr = jsdata [$(this).attr ('tkey')];
+	    $(this).html (strTr);
+	});
+}
+
+
+langCode = navigator.language.substr (0, 2);
+document.write(langCode);
+
+if (langCode == 'es')
+	//$.getJSON('libs/multilanguage/lang/'+langCode+'.json', translate);
+	$.getJSON('libs/multilanguage/lang/es.json', translate);  
+else
+	$.getJSON('libs/multilanguage/lang/en.json', translate);
+
+
+
