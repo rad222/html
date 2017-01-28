9afee62b3e0604db17e873b7b29540f0dd8da7dc
Javier <rad222@users.noreply.github.com>
2017-01-28 12:57:27 +0100

    Update main.js

--- app/libs/main.js
+++ app/libs/main.js
@@ -655,7 +655,7 @@ info.onAdd = function (map) {
 };
 
 info.update = function (content) {
-	this._div.innerHTML = "<p tkey="Information">Information:</p><a id='some_id' class='leaflet-popup-close-button' href='#' onclick='return hideInfo();'>×</a><br>" + content;
+	this._div.innerHTML = "<b tkey="information">Information:</b><a id='some_id' class='leaflet-popup-close-button' href='#' onclick='return hideInfo();'>×</a><br>" + content;
 };
 
 info.addTo(map);
