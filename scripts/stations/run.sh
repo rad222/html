de8c642c6e3fb1dc5ca4bed2bd27e76bf362f9eb
spatialhast <spatial.hast@gmail.com>
2017-04-02 10:08:01 +0300

    Update stations script

--- scripts/stations/run.sh
+++ scripts/stations/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 # project path
 PROJECT_PATH=/home/radon/html
 
@@ -10,22 +12,24 @@ STATIONS_PATH=/home/radon/html/scripts/stations
 for scode in AT BE BG CH CY CZ DE DK EE ES FI FR GB GL GR HR HU IE IS IT LT LU LV MK MT NL NO PL PT RO RS RU SE SI SK TR UA 
 do
 	SCODE=$scode
-	echo ${SCODE}
+	#echo ${SCODE}
 	#python ${STATIONS_PATH}/test_eurdep_single.py ${SCODE}
 done
 
 
 
-OutFileName="X.csv"                       # Fix the output name
-i=0                                       # Reset a counter
+# merge csv files in one
+STATIONS_DATE=$(date +"%Y%m%d%H%M")
+OUTPUT_STATIONS="${STATIONS_PATH}/outputs/$STATIONS_DATE.csv"
+i=0
 for filename in ${STATIONS_PATH}/inputs/*.csv; do 
- if [ "$filename"  != "$OutFileName" ] ;      # Avoid recursion 
+ if [ "$filename"  != "$OUTPUT_STATIONS" ] ;
  then 
    if [[ $i -eq 0 ]] ; then 
-      head -1  $filename >   $OutFileName # Copy header if it is the first file
+      head -1  $filename >   $OUTPUT_STATIONS
    fi
-   tail -n +2  $filename >>  $OutFileName # Append from the 2nd line each file
-   i=$(( $i + 1 ))                        # Increase the counter
+   tail -n +2  $filename >>  $OUTPUT_STATIONS
+   i=$(( $i + 1 ))
  fi
 done
 
