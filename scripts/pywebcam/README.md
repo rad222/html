bc837da54a3b412ca4f89226865dd25260f446a1
Alexander <spatial.hast@gmail.com>
2017-04-08 19:06:39 +0300

    add  munimadrid

--- scripts/pywebcam/README.md
+++ scripts/pywebcam/README.md
@@ -24,6 +24,7 @@ $ cd ~/html/scripts/pywebcam
 $ python pywebcam.py DGT
 $ python pywebcam.py metcli
 $ python pywebcam.py tiempovistazo
+$ python pywebcam.py munimadrid
 ```
 ```sh
 # run all
--- scripts/pywebcam/run.sh
+++ scripts/pywebcam/run.sh
@@ -9,14 +9,14 @@ PROJECT_PATH=/home/radon/html
 PYWEBCAM_PATH=/home/radon/html/scripts/pywebcam
 
 # parse webcam data 
-for ccode in DGT metcli tiempovistazo
+for ccode in DGT metcli tiempovistazo munimadrid
 do
 	CCODE=$ccode
 	echo ${CCODE}
 	python ${PYWEBCAM_PATH}/pywebcam.py ${CCODE}
 done
 
-# merge 'DGT metcli tiempovistazo' to 'webcams.csv' with webcam 'type'
+# merge 'DGT metcli tiempovistazo munimadrid' to 'webcams.csv' with webcam 'type'
 sed 1d ${PYWEBCAM_PATH}/webcams_DGT.csv > ${PYWEBCAM_PATH}/webcams_DGT_.csv
 sed 1d ${PYWEBCAM_PATH}/webcams_metcli.csv > ${PYWEBCAM_PATH}/webcams_metcli_.csv
 sed 1d ${PYWEBCAM_PATH}/webcams_tiempovistazo.csv > ${PYWEBCAM_PATH}/webcams_tiempovistazo_.csv
@@ -24,25 +24,26 @@ sed 1d ${PYWEBCAM_PATH}/webcams_tiempovistazo.csv > ${PYWEBCAM_PATH}/webcams_tie
 sed 's/$/|DGT/' ${PYWEBCAM_PATH}/webcams_DGT_.csv > ${PYWEBCAM_PATH}/webcams_DGT.csv
 sed 's/$/|metcli/' ${PYWEBCAM_PATH}/webcams_metcli_.csv > ${PYWEBCAM_PATH}/webcams_metcli.csv
 sed 's/$/|tiempovistazo/' ${PYWEBCAM_PATH}/webcams_tiempovistazo_.csv > ${PYWEBCAM_PATH}/webcams_tiempovistazo.csv
+sed 's/$/|munimadrid/' ${PYWEBCAM_PATH}/webcams_tiempovistazo_.csv > ${PYWEBCAM_PATH}/webcams_munimadrid.csv
 
 rm ${PYWEBCAM_PATH}/webcams_DGT_.csv
 rm ${PYWEBCAM_PATH}/webcams_metcli_.csv
 rm ${PYWEBCAM_PATH}/webcams_tiempovistazo_.csv
 
-cat ${PYWEBCAM_PATH}/webcams_DGT.csv ${PYWEBCAM_PATH}/webcams_metcli.csv ${PYWEBCAM_PATH}/webcams_tiempovistazo.csv > ${PYWEBCAM_PATH}/webcams.csv
+cat ${PYWEBCAM_PATH}/webcams_DGT.csv ${PYWEBCAM_PATH}/webcams_metcli.csv ${PYWEBCAM_PATH}/webcams_tiempovistazo.csv ${PYWEBCAM_PATH}/webcams_munimadrid.csv > ${PYWEBCAM_PATH}/webcams.csv
 
 sed -i 1i'id|lon|lat|img|type' ${PYWEBCAM_PATH}/webcams.csv
 
 rm ${PYWEBCAM_PATH}/webcams_DGT.csv
 rm ${PYWEBCAM_PATH}/webcams_metcli.csv
 rm ${PYWEBCAM_PATH}/webcams_tiempovistazo.csv
+rm ${PYWEBCAM_PATH}/webcams_munimadrid.csv
 
 #chmod -R 777 ${PYWEBCAM_PATH}
 #chown -R radon:radon ${PYWEBCAM_PATH}
 
 # copy 'webcams.csv' to web app folder
 #cp -R ${PYWEBCAM_PATH}/webcams.csv /var/www/html
-
 
 # copy 'webcams.csv' to 'html/app/data' folder
 cp -R ${PYWEBCAM_PATH}/webcams.csv /home/radon/html/app/data
