7ac994b69c6babe03403b48d6a3768d8ade075f7
spatialhast <spatial.hast@gmail.com>
2017-04-14 17:05:54 +0300

    add stations to sqlite script

--- scripts/stations/csv_to_sqlite.py
+++ scripts/stations/csv_to_sqlite.py
@@ -2,9 +2,14 @@
 
 # python ~/html/scripts/stations/csv_to_sqlite.py
 
+# sudo apt-get install sqlite
+
 import os
 import logging
-from pyspatialite import dbapi2 as db
+import sqlite3
+
+# http://proft.me/2009/05/21/python-i-baza-dannyh-sqlite/
+# https://docs.python.org/2/library/sqlite3.html
 
 
 # base dir
@@ -12,13 +17,23 @@ base_dir = os.path.realpath(os.path.dirname(__file__))
 
 # logger
 logger = logging.getLogger('myapp')
-hdlr = logging.FileHandler(os.path.join(base_dir, "log.log"))
+hdlr = logging.FileHandler(os.path.join(base_dir, "stations.log"))
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
 logger.addHandler(hdlr) 
 logger.setLevel(logging.INFO)
 
+con = sqlite3.connect(os.path.join(base_dir, "stations.db"))
+cur = con.cursor()
+cur.execute('CREATE TABLE users (id INTEGER PRIMARY KEY, firstName VARCHAR(100), secondName VARCHAR(30))')
+con.commit()
+cur.execute('INSERT INTO users (id, firstName, secondName) VALUES(NULL, "Guido", "van Rossum")')
+con.commit()
+print cur.lastrowid
 
+cur.execute('SELECT * FROM users')
+print cur.fetchall()
+con.close()
 
 
 
