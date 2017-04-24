f1d95d095973f99425df3f2cdbee8e6789177d43
spatialhast <spatial.hast@gmail.com>
2017-04-24 11:35:29 +0300

    update stations data (24/04/2017 11:35)

--- scripts/pywebcam/run.sh
+++ scripts/pywebcam/run.sh
@@ -22,10 +22,10 @@ sed 1d ${PYWEBCAM_PATH}/webcams_metcli.csv > ${PYWEBCAM_PATH}/webcams_metcli_.cs
 sed 1d ${PYWEBCAM_PATH}/webcams_tiempovistazo.csv > ${PYWEBCAM_PATH}/webcams_tiempovistazo_.csv
 sed 1d ${PYWEBCAM_PATH}/webcams_munimadrid.csv > ${PYWEBCAM_PATH}/webcams_munimadrid_.csv
 
-sed 's/$/#DGT/' ${PYWEBCAM_PATH}/webcams_DGT_.csv > ${PYWEBCAM_PATH}/webcams_DGT.csv
-sed 's/$/#metcli/' ${PYWEBCAM_PATH}/webcams_metcli_.csv > ${PYWEBCAM_PATH}/webcams_metcli.csv
-sed 's/$/#tiempovistazo/' ${PYWEBCAM_PATH}/webcams_tiempovistazo_.csv > ${PYWEBCAM_PATH}/webcams_tiempovistazo.csv
-sed 's/$/#munimadrid/' ${PYWEBCAM_PATH}/webcams_munimadrid_.csv > ${PYWEBCAM_PATH}/webcams_munimadrid.csv
+sed 's/$/^DGT/' ${PYWEBCAM_PATH}/webcams_DGT_.csv > ${PYWEBCAM_PATH}/webcams_DGT.csv
+sed 's/$/^metcli/' ${PYWEBCAM_PATH}/webcams_metcli_.csv > ${PYWEBCAM_PATH}/webcams_metcli.csv
+sed 's/$/^tiempovistazo/' ${PYWEBCAM_PATH}/webcams_tiempovistazo_.csv > ${PYWEBCAM_PATH}/webcams_tiempovistazo.csv
+sed 's/$/^munimadrid/' ${PYWEBCAM_PATH}/webcams_munimadrid_.csv > ${PYWEBCAM_PATH}/webcams_munimadrid.csv
 
 rm ${PYWEBCAM_PATH}/webcams_DGT_.csv
 rm ${PYWEBCAM_PATH}/webcams_metcli_.csv
@@ -34,12 +34,12 @@ rm ${PYWEBCAM_PATH}/webcams_munimadrid_.csv
 
 cat ${PYWEBCAM_PATH}/webcams_DGT.csv ${PYWEBCAM_PATH}/webcams_metcli.csv ${PYWEBCAM_PATH}/webcams_tiempovistazo.csv ${PYWEBCAM_PATH}/webcams_munimadrid.csv > ${PYWEBCAM_PATH}/webcams.csv
 
-sed -i 1i'id#lon#lat#img#type' ${PYWEBCAM_PATH}/webcams.csv
+sed -i 1i'id^lon^lat^img^type' ${PYWEBCAM_PATH}/webcams.csv
 
-rm ${PYWEBCAM_PATH}/webcams_DGT.csv
-rm ${PYWEBCAM_PATH}/webcams_metcli.csv
-rm ${PYWEBCAM_PATH}/webcams_tiempovistazo.csv
-rm ${PYWEBCAM_PATH}/webcams_munimadrid.csv
+#rm ${PYWEBCAM_PATH}/webcams_DGT.csv
+#rm ${PYWEBCAM_PATH}/webcams_metcli.csv
+#rm ${PYWEBCAM_PATH}/webcams_tiempovistazo.csv
+#rm ${PYWEBCAM_PATH}/webcams_munimadrid.csv
 
 #chmod -R 777 ${PYWEBCAM_PATH}
 #chown -R radon:radon ${PYWEBCAM_PATH}
@@ -51,6 +51,6 @@ rm ${PYWEBCAM_PATH}/webcams_munimadrid.csv
 cp -R ${PYWEBCAM_PATH}/webcams.csv /home/radon/html/app/data
 
 # push changes
-git --git-dir=$PROJECT_PATH/.git --work-tree=$PROJECT_PATH add .
-git --git-dir=$PROJECT_PATH/.git --work-tree=$PROJECT_PATH commit -a -m "update webcams data (`date +%d/%m/%Y' '%H:%M`)"
-git --git-dir=$PROJECT_PATH/.git --work-tree=$PROJECT_PATH push origin master
+#git --git-dir=$PROJECT_PATH/.git --work-tree=$PROJECT_PATH add .
+#git --git-dir=$PROJECT_PATH/.git --work-tree=$PROJECT_PATH commit -a -m "update webcams data (`date +%d/%m/%Y' '%H:%M`)"
+#git --git-dir=$PROJECT_PATH/.git --work-tree=$PROJECT_PATH push origin master
